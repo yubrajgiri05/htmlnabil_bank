1fe32705537958322e52ec1aa3c529444bab324a
yubrajgiri05 <giriyubraj787898@gmail.com>
2023-04-18 21:51:36 +0545

    Updates

---
+++ precache-manifest.2b86653963e0399f4ec394439ce393e3.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cada87124699506c8449eedce328e711",
+    "url": "/My_Portfolio/index.html"
+  },
+  {
+    "revision": "6b4ede1899b4c350c1c1",
+    "url": "/My_Portfolio/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "d7a65591c209c3f23d13",
+    "url": "/My_Portfolio/static/css/main.d8cda600.chunk.css"
+  },
+  {
+    "revision": "6b4ede1899b4c350c1c1",
+    "url": "/My_Portfolio/static/js/2.7d2f465a.chunk.js"
+  },
+  {
+    "revision": "e53cf286e00aab3498ae2b2d3185567a",
+    "url": "/My_Portfolio/static/js/2.7d2f465a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7a65591c209c3f23d13",
+    "url": "/My_Portfolio/static/js/main.9a5b07f2.chunk.js"
+  },
+  {
+    "revision": "0378652d64bab1f36f52",
+    "url": "/My_Portfolio/static/js/runtime-main.81491c15.js"
+  },
+  {
+    "revision": "5b67f91f24bfdcfc9453ee57ee4af393",
+    "url": "/My_Portfolio/static/media/AndroidStudio-Intro-feature.5b67f91f.png"
+  },
+  {
+    "revision": "86dcc26e54aeb4da1001a2b9a32839bb",
+    "url": "/My_Portfolio/static/media/Custom-Software-Development.86dcc26e.jpg"
+  },
+  {
+    "revision": "c5a2d6e272d729f2706a9d09b864aad3",
+    "url": "/My_Portfolio/static/media/digital-marketing.c5a2d6e2.png"
+  },
+  {
+    "revision": "f3668a1df5e7c0e7270d9b18d44f0870",
+    "url": "/My_Portfolio/static/media/home.f3668a1d.svg"
+  },
+  {
+    "revision": "c0a3c656af08ed90fb4fb45481075bcd",
+    "url": "/My_Portfolio/static/media/mad_blog_5df2190b8638a1576147211.c0a3c656.jpg"
+  },
+  {
+    "revision": "52760becb07b61214e9e7eb0036a497f",
+    "url": "/My_Portfolio/static/media/marketing-plan_1.52760bec.jpg"
+  },
+  {
+    "revision": "1a567371032ffb1b69c5d719f79f5899",
+    "url": "/My_Portfolio/static/media/technical-support-137-1087325.1a567371.png"
+  },
+  {
+    "revision": "089c6f85929620dfab10728afb9fe828",
+    "url": "/My_Portfolio/static/media/web.089c6f85.png"
+  }
+]);
